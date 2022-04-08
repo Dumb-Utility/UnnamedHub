45c408e25f088c7749e1b0295e703c5dedb8e500
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-04-08 23:17:21 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,15 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 
+local DevBuild = false
+
 function notify(Message)
 	game:GetService("StarterGui"):SetCore("SendNotification", { 
-		Title = "UnnamedHub";
+		Title = "UnnamedHub Beta";
 		Text = Message;
 		Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
 end
 
-if game:GetService("Players").LocalPlayer.UserId ~= 2820225419 then notify("UnnamedHub isn't ready for release yet.") return end
+if game:GetService("Players").LocalPlayer.UserId ~= 2820225419 and DevBuild == true then notify("UnnamedHub isn't ready for release yet.") return end
 
 notify("Welcome "..game:GetService("Players").LocalPlayer.Name)
 
