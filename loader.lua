d7128b8489a89c8bdfac444c3e6db711843dd2e6
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-04-08 23:09:32 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,3 +10,14 @@ end
 if game:GetService("Players").LocalPlayer.UserId ~= 2820225419 then notify("UnnamedHub isn't ready for release yet.") return end
 
 notify("Welcome "..game:GetService("Players").LocalPlayer.Name)
+
+local games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedHub/main/games.lua"))()
+
+print(games.games)
+
+for _,v in pairs(games.games) do
+  if v == game.PlaceId then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedHub/main/games/2248408710"...tostring(game.PlaceId).."lua"))()
+    break
+  end
+end
