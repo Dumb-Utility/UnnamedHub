ceaf0c035cc22bbacffaa3818e9cb188845aba72
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-04-14 22:43:33 +0200

    Updated Pro Border

--- games/7014716500.lua
+++ games/7014716500.lua
@@ -1,1 +1,5 @@
--- https://www.roblox.com/games/7014716500/UPDATE-Pro-Border
+-- https://www.roblox.com/games/7014716500/Pro-Border
+
+local gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedHub/main/UI.lua"))()
+
+gui.CreateGui()
