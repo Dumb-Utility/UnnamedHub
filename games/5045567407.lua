2da120606c476173a5ee8e414fe895d6e61c07dd
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-04-15 18:31:59 +0200

    Added gay game 4000 (Nations Roleplay)

--- games/5045567407.lua
+++ games/5045567407.lua
@@ -1,1 +1,34 @@
 -- https://www.roblox.com/games/5045567407/Nations-Roleplay
+
+local LP = game:GetService("Players").LocalPlayer
+local gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedHub/main/UI.lua"))()
+
+gui.CreateGui()
+
+AddFrame("Yes")
+
+AddButton("Yes", "Change colors", function()
+    local RGB = Color3.fromRGB(GetString("Yes", "RGB"))
+    if not LP.Character:FindFirstChild("PaintBucket") then LP.Backpack.PaintBucket.Parent = LP.Character warn("Auto equipped the PaintBucket") end
+
+for i, v in pairs(game:GetService("Workspace").Provinces:GetChildren()) do
+   task.spawn(function()
+   local args = {
+       [1] = "PaintPart",
+       [2] = {
+           ["Part"] = v,
+           ["Color"] = RGB
+       }
+   }
+   
+   -- Double check cuz I felt like it
+   if not LP.Character:FindFirstChild("PaintBucket") then LP.Backpack.PaintBucket.Parent = LP.Character warn("Auto equipped the PaintBucket") end
+   
+   LP.Character.PaintBucket.Remotes.ServerControls:InvokeServer(unpack(args))
+   end)
+  end
+end)
+
+AddTextBox("Yes", "RGB")
+  
+AddButton("Yes", "Just that yea", function() end)
