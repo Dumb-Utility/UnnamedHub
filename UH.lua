107426189b1f1cb390b81d74677671f1f0e4b23d
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-05-18 15:53:30 +0200

    Updated UH.lua

--- UH.lua
+++ UH.lua
@@ -22,6 +22,29 @@ function notify(Message)
 		Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
 end
 
+function promtDicordInvite(inv)
+    	local httpService = game:GetService("HttpService")
+    	local httpRequest = (syn and syn.request) or (httpService and httpService.request) or (http_request)
+    
+    	if not httpRequest then if setclipboard then setclipboard("https://discord.gg/"..inv) end  if toclipboard then toclipboard("https://discord.gg/"..inv) end notify("Invite copied to clipboard.")  return end
+    	
+    	    httpRequest({
+        		Url = "http://127.0.0.1:6463/rpc?v=1",
+        		Method = "POST",
+        
+        		Headers = {
+        			['Content-Type'] = 'application/json',
+        			Origin = 'https://discord.com'
+        		},
+        
+        		Body = httpService:JSONEncode({
+        			cmd = 'INVITE_BROWSER',
+        			nonce = httpService:GenerateGUID(false),
+        			args = {code = inv}
+        		})
+        	})
+end
+
 if game:GetService("Players").LocalPlayer.UserId ~= 2820225419 and DevBuild == true then notify("UnnamedHub isn't ready for release yet.") return end
 
 local games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedHub/main/games.lua"))()
@@ -33,6 +56,28 @@ for _,v in pairs(games.games) do
         --warn("UnnamedHub loaded !")
 	notify("Press Right Shift to show the ui")
         loaded = true
+	local Bindable = Instance.new("BindableFunction")
+Bindable.OnInvoke = function(who, buttonpressed)
+	--print("invoke received by "..who)
+		if who == "Yes" then
+		    promtDicordInvite("3eEAN4Qr42")
+		else
+		    
+		end
+end
+
+game:GetService("StarterGui"):SetCore(
+		"SendNotification",
+		{
+			Title = "UnnamedHub",
+			Duration = 10,
+			Text = "Do you want to join the discord server?",
+			Button1 = "Yes",
+			Button2 = "No",
+			Callback = Bindable
+		}
+	)
+end
     break
   end
 end
