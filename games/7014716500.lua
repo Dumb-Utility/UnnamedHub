aed1664bd855b7a64069efb97cba1e0b78958639
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-06-23 21:20:14 +0200

    Fixed UI | Pro Border

--- games/7014716500.lua
+++ games/7014716500.lua
@@ -16,8 +16,6 @@ local Window = gui:CreateGui()
 
 local team = Window:AddFrame("Teams")
 
-team:AddButton("Works outside of menu", function() end)
-
 team:AddButton("Civilian", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("Civilian") LP.Character:WaitForChild("Humanoid").Health = 0 end)
 
 team:AddButton("Facility Director", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("FacilityDirector") LP.Character:WaitForChild("Humanoid").Health = 0 end)
@@ -26,11 +24,11 @@ team:AddButton("Guard", function() game:GetService("ReplicatedStorage").RemoteEv
 
 team:AddButton("Medic", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("MedicalStaff") LP.Character:WaitForChild("Humanoid").Health = 0 end)
 
-team:AddButton("Engineer Guy", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("MaintenanceCrew") LP.Character:WaitForChild("Humanoid").Health = 0 end)
+team:AddButton("Engineer", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("MaintenanceCrew") LP.Character:WaitForChild("Humanoid").Health = 0 end)
 
-team:AddButton("Scientist (Science team)", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("Scientist") LP.Character:WaitForChild("Humanoid").Health = 0 end)
+team:AddButton("Scientist", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("Scientist") LP.Character:WaitForChild("Humanoid").Health = 0 end)
 
-team:AddButton("MTF but with a different name", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("EmergencyResponse") LP.Character:WaitForChild("Humanoid").Health = 0 end)
+team:AddButton("Task Force", function() game:GetService("ReplicatedStorage").RemoteEvents.UpdateTeam:FireServer("EmergencyResponse") LP.Character:WaitForChild("Humanoid").Health = 0 end)
 
 local gun = Window:AddFrame("Gun")
 gun:AddButton("Mod Guns", function()
@@ -39,9 +37,8 @@ for _,v in pairs(LP.Backpack:GetChildren()) do
     if v.ToolType.Value == "Gun" then	
 local gun = v.ToolType
 gun.Auto.Value = true
-gun.Ammo.Value = 9999
-gun.MaxAmmo.Value = 9999
-gun.ReserveAmmo.Value = 6969420
+gun.MaxAmmo.Value = math.huge
+gun.ReserveAmmo.Value = math.huge
     end
   end
 end
