3597fa728c09f5c8f5c4104fcb29d646d8bfe2e2
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-09-16 23:04:54 +0200

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,5 +1,27 @@
 local Global = getgenv and getgenv() or _G
 
+function TableRemove(tab: table, arg: any)
+	--print("remove", tab)
+    for n,v in pairs(tab) do
+        if v == arg then
+			--print("removed", v)
+			table.remove(tab, n)
+		end
+    end
+	return tab
+end
+
+function TableAdd(tab: table, arg: any, pos: number?)
+	--print("add", tab)
+	if pos then
+		table.insert(tab, arg, pos)
+	else
+	--print("ok for", arg)
+    table.insert(tab, arg)
+	end
+	return tab
+end
+
 module = {}
 function module:CreateGui(name)
 	local Window = {}
@@ -36,18 +58,35 @@ function module:CreateGui(name)
 	local UICorner_2 = Instance.new("UICorner")
 	local UICorner_3 = Instance.new("UICorner")
 	local UICorner_4 = Instance.new("UICorner")
+	local DropBoxTemplate = Instance.new("TextButton")
+	local Open_3 = Instance.new("ImageButton")
+	local DropFrame = Instance.new("Frame")
+	local Scrolling = Instance.new("ScrollingFrame")
+	local ScrollingUI = Instance.new("UIListLayout")
+	local UICorner_5 = Instance.new("UICorner")
+	local DropButton = Instance.new("TextButton")
+	local UICorner_6 = Instance.new("UICorner")
+	local TextLabel = Instance.new("TextLabel")
+	local DropCheck = Instance.new("TextButton")
+    local Check = Instance.new("ImageButton")
+    local CheckCorner = Instance.new("UICorner")
+    local CheckCorner1 = Instance.new("UICorner")
 
 	--Properties:
+	local Par = game:GetService("CoreGui")
 	local synprotect = nil
-	if syn then
+	if (syn and syn.protect_gui) then
 		synprotect = syn.protect_gui 
 	end
-	if synprotect then
+	if synprotect and not gethui then
 		synprotect(ScreenGui)
-	end
+        end
+	if gethui then
+            Par = gethui()
+        end
 
 	ScreenGui.Name = "UH"
-	ScreenGui.Parent = game:GetService("CoreGui")
+	ScreenGui.Parent = Par
 	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 	ScreenGui.ResetOnSpawn = false
 	ScreenGui.IgnoreGuiInset = true
@@ -158,7 +197,7 @@ function module:CreateGui(name)
 	NumberBoxTemplate.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	NumberBoxTemplate.BackgroundTransparency = 1.000
 	NumberBoxTemplate.BorderSizePixel = 0
-	NumberBoxTemplate.Position = UDim2.new(0.035, 0, 7.118, 0)
+	NumberBoxTemplate.Position = UDim2.new(0.029, 0, 7.118, 0)
 	NumberBoxTemplate.Size = UDim2.new(0, 168, 0, 29)
 	NumberBoxTemplate.Font = Enum.Font.Jura
 	NumberBoxTemplate.Text = "NumberBox"
@@ -200,7 +239,7 @@ function module:CreateGui(name)
 	StringBoxTemplate.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	StringBoxTemplate.BackgroundTransparency = 1.000
 	StringBoxTemplate.BorderSizePixel = 0
-	StringBoxTemplate.Position = UDim2.new(0.035, 0, 5.235, 0)
+	StringBoxTemplate.Position = UDim2.new(0.029, 0, 5.235, 0)
 	StringBoxTemplate.Size = UDim2.new(0, 168, 0, 29)
 	StringBoxTemplate.Font = Enum.Font.Jura
 	StringBoxTemplate.Text = "TextBox"
@@ -237,6 +276,97 @@ function module:CreateGui(name)
 
 	UICorner_4.CornerRadius = UDim.new(0, 5)
 	UICorner_4.Parent = TextBox_2
+	
+	DropBoxTemplate.Name = "DropBoxTemplate"
+	DropBoxTemplate.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	DropBoxTemplate.BackgroundTransparency = 1.000
+	DropBoxTemplate.BorderSizePixel = 0
+	DropBoxTemplate.Position = UDim2.new(0.029, 0, 0.528528571, 0)
+	DropBoxTemplate.Size = UDim2.new(0, 168, 0, 29)
+	DropBoxTemplate.Font = Enum.Font.Jura
+	DropBoxTemplate.Text = "NumberBox"
+	DropBoxTemplate.TextColor3 = Color3.fromRGB(255, 255, 255)
+	DropBoxTemplate.TextSize = 18.000
+	DropBoxTemplate.TextXAlignment = Enum.TextXAlignment.Left
+
+	Open_3.Name = "Open"
+	Open_3.Parent = DropBoxTemplate
+	Open_3.BackgroundTransparency = 1.000
+	Open_3.Position = UDim2.new(0.827380896, 0, 0.12068966, 0)
+	Open_3.Size = UDim2.new(0, 25, 0, 25)
+	Open_3.ZIndex = 2
+	Open_3.Image = "rbxassetid://3926305904"
+	Open_3.ImageRectOffset = Vector2.new(564, 284)
+	Open_3.ImageRectSize = Vector2.new(36, 36)
+
+	DropFrame.Name = "DropFrame"
+	DropFrame.Visible = false
+	DropFrame.Parent = DropBoxTemplate
+	DropFrame.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
+	DropFrame.Position = UDim2.new(1.11309528, 0, -0.448276401, 0)
+	DropFrame.Size = UDim2.new(0, 177, 0, 100)
+	
+	Scrolling.Name = "Scrolling"
+	Scrolling.Parent = DropFrame
+	Scrolling.Active = true
+	Scrolling.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
+	Scrolling.BorderSizePixel = 0
+	Scrolling.Position = UDim2.new(0.0229015462, 0, 0.0199993905, 0)
+	Scrolling.Size = UDim2.new(0, 167, 0, 98)
+	Scrolling.CanvasSize = UDim2.new(0, 0, 0.899999976, 0)
+	Scrolling.ScrollBarThickness = 5
+
+	ScrollingUI.Name = "ScrollingUI"
+	ScrollingUI.Parent = Scrolling
+	ScrollingUI.SortOrder = Enum.SortOrder.LayoutOrder
+	ScrollingUI.Padding = UDim.new(0, 4)
+
+	UICorner_5.Parent = DropFrame
+
+	DropButton.Name = "DropButton"
+	DropButton.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+	DropButton.Size = UDim2.new(0, 159, 0, 23)
+	DropButton.Font = Enum.Font.GothamMedium
+	DropButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+	DropButton.TextSize = 14.000
+	
+	TextLabel.Parent = Open_3
+	TextBox.Text = "None"
+	TextLabel.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
+	TextLabel.Position = UDim2.new(-5.71999979, 0, 0.999999404, 0)
+	TextLabel.Size = UDim2.new(0, 173, 0, 17)
+	TextLabel.Font = Enum.Font.GothamMedium
+	TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextLabel.TextSize = 12.000
+
+	UICorner_6.Parent = DropButton
+
+	DropCheck.Name = "CheckBox"
+	DropCheck.Visible = false
+	DropCheck.Parent = DropButton
+	DropCheck.BackgroundColor3 = Color3.fromRGB(103, 103, 103)
+	DropCheck.Position = UDim2.new(0.817003667, 0, 0.146585941, 0)
+	DropCheck.Size = UDim2.new(0, 20, 0, 20)
+	DropCheck.Font = Enum.Font.SourceSans
+	DropCheck.Text = ""
+	DropCheck.TextColor3 = Color3.fromRGB(0, 0, 0)
+	DropCheck.TextSize = 14.000
+
+    Check.Name = "check"
+	Check.Parent = DropCheck
+	Check.BackgroundTransparency = 1.000
+	Check.Position = UDim2.new(-0.0315256119, 0, 0, 0)
+	Check.Size = UDim2.new(0, 23, 0, 23)
+	Check.Visible = false
+	Check.ZIndex = 2
+	Check.Image = "rbxassetid://3926305904"
+	Check.ImageRectOffset = Vector2.new(312, 4)
+	Check.ImageRectSize = Vector2.new(24, 24)
+
+    CheckCorner.Parent = DropCheck
+
+	CheckCorner1.Parent = Check
+	
 	-- Scripts:
 	local LastPos = 0
 	local First = 1
@@ -269,7 +399,7 @@ function module:CreateGui(name)
 		end
 		LastPos = Cop.Position
 		if Size ~= nil then
-			Cop.Size = UDim2.fromOffset(Cop.Size.X.Offset, Size)
+			Cop:WaitForChild("CategoryFrame").Size = UDim2.fromOffset(Cop:WaitForChild("CategoryFrame").Size.X.Offset, Size)
 		end
 		Cop:SetAttribute("First", 0)
 		local e = Instance.new("ObjectValue", Cop)
@@ -289,6 +419,7 @@ function module:CreateGui(name)
 
 		local Thing = Cop
 		function Tab:AddButton(Name, Run)
+			Run = Run or function() end
 			local Thinge = Thing:WaitForChild("Content")
 			local FirstBut = Thing:GetAttribute("First")
 			local LastBut = Thing:WaitForChild("LastBut").Value
@@ -322,8 +453,9 @@ function module:CreateGui(name)
 			end)
 		end
 
-		function Tab:AddTextBox(Name)
+		function Tab:AddTextBox(Name, callback)
 			local Box = {}
+			callback = callback or function() end
 			Thinge = Thing:WaitForChild("Content")
 			local FirstBut = Thing:GetAttribute("First")
 			local LastBut = Thing:WaitForChild("LastBut").Value
@@ -352,6 +484,11 @@ function module:CreateGui(name)
 			end
 			Thing:WaitForChild("LastBut").Value = but
 			-- print(Thing:WaitForChild("LastBut").Value)
+			
+			but.MouseButton1Click:Connect(function()
+				callback(but.Open.TextBox.Text)
+			end)
+			
 			but.Open.MouseButton1Click:Connect(function()
 				but.Open.TextBox.Visible = not but.Open.TextBox.Visible
 			end)
@@ -365,6 +502,7 @@ function module:CreateGui(name)
 
 		function Tab:AddNumberBox(Name, Run)
 			local Num = {}
+			Run = Run or function() end
 			Thinge = Thing:WaitForChild("Content")
 			local FirstBut = Thing:GetAttribute("First")
 			local LastBut = Thing:WaitForChild("LastBut").Value
@@ -403,7 +541,7 @@ function module:CreateGui(name)
 			but.MouseButton1Click:Connect(function()
 				if but.Open.TextBox.Text == "" then return end
 				local Number = tonumber(but.Open.TextBox.Text)
-				Run()
+				Run(Number)
 			end)
 			function Num:GetNumber()
 				h = but.Open.TextBox
@@ -413,7 +551,8 @@ function module:CreateGui(name)
 			return Num
 		end
 
-		function Tab:AddCheckBox(Name, On, Off)
+		function Tab:AddCheckBox(Name, callback)
+			callback = callback or function() end
 			Thinge = Thing:WaitForChild("Content")
 			local FirstBut = Thing:GetAttribute("First")
 			local LastBut = Thing:WaitForChild("LastBut").Value
@@ -446,18 +585,138 @@ function module:CreateGui(name)
 			but:WaitForChild("CheckBox").MouseButton1Click:Connect(function()
 				but.CheckBox.check.Visible = not but.CheckBox.check.Visible
 				-- Using not to fix the thing going opposite (on = off, off = on)
-				local activ = not but.CheckBox.check.Visible
-				if activ then
-					On()
-				else
-					Off()
-				end
+				local activ = but.CheckBox.check.Visible
+				callback(activ)
 			end)
 
 		end
+		
+		function Tab:AddDropBox(Name, callback, Check)
+			local Box = {};
+			callback = callback or function() end
+		    if not type(Check) == "boolean" then Check = false end
+			Thinge = Thing:WaitForChild("Content")
+			local FirstBut = Thing:GetAttribute("First")
+			local LastBut = Thing:WaitForChild("LastBut").Value
+			local but = DropBoxTemplate:Clone()
+			local CurElement
+			if Check == true then CurElement = {} end
+			but.Parent = Thinge
+			but.Name = Name
+			but.Text = Name
+			if FirstBut ~= 0 then
+				if FirstBut == 1 then
+					if LastBut:FindFirstChild("SubName") then
+						but.Position = UDim2.fromOffset(7, LastBut.Position.Y.Offset + 70)
+					else
+						but.Position = UDim2.fromOffset(7, LastBut.Position.Y.Offset + 90)
+					end
+					Thing:SetAttribute("First", 2)
+				else
+					if LastBut:FindFirstChild("SubName") then
+						but.Position = UDim2.fromOffset(7, LastBut.Position.Y.Offset + 20)
+					else
+						but.Position = UDim2.fromOffset(7, LastBut.Position.Y.Offset + 40)
+					end
+				end
+				--but.Position = UDim2.fromScale(LastBut.Position.X.Scale)
+			else
+				Thing:SetAttribute("First", 1)
+			end
+			Thing:WaitForChild("LastBut").Value = but
+			-- print(Thing:WaitForChild("LastBut").Value)
+			
+			local function Update(New)
+				local TextLab = but.Open.TextLabel
+				local Old     = TextLab.Text
+				if New == Old and Check == false then
+					TextLab.Text = "None"
+					CurElement     = nil
+					return
+				end
+				TextLab.Text = New
+			end
+			
+			local function SizeOf(Name, Loc)
+				local siz = 0
+				for _,v in pairs(Loc) do
+					if v.Name == Name then siz += 1 end
+				end
+				return siz
+			end
+			
+			but.Open.MouseButton1Click:Connect(function()
+				but.DropFrame.Visible = not but.DropFrame.Visible
+			end)
+			
+			function Box:GetSelected()
+				return CurElement
+			end
+			
+			but.MouseButton1Click:Connect(function()
+				local selected = Box:GetSelected()
+				callback(selected)
+			end)
+			
+			function Box:AddElement(Name, Value)
+				local NewEl = DropButton:Clone()
+				-- Avoid Clones
+				NewEl.Name = Name
+				if but.DropFrame.Scrolling:FindFirstChild(Name) then
+					NewEl.Name = Name.."_"..tostring(SizeOf(Name, but.DropFrame.Scrolling)+1)
+				end
+				if Check then
+					NewEl.CheckBox.Visible = true
+				end
+				NewEl.Text = NewEl.Name
+				NewEl.Parent = but.DropFrame.Scrolling
+				NewEl.MouseButton1Click:Connect(function()
+					if Check == false then
+					if but.Open.TextLabel.Text == NewEl.Text then return end
+					end
+					Update(NewEl.Name)
+					if Check then
+						local fix
+						NewEl.CheckBox.check.Visible = not NewEl.CheckBox.check.Visible
+				        local activ = NewEl.CheckBox.check.Visible
+						--print(activ)
+						if activ == true then
+							fix = TableAdd
+						else
+							fix = TableRemove
+						end
+                      if typeof(Value) == "table" then
+						 for _,ok in pairs(Value) do
+							CurElement = fix(CurElement, ok)
+						 end
+						else
+							--print(CurElement)
+							CurElement = fix(CurElement, Value)
+							--print("debuggg", CurElement)
+						end
+						--print("new", CurElement)
+					else
+					CurElement = Value
+					end
+					callback(CurElement)
+				end)
+			end
+
+			function Box:RemoveElement(element)
+				local el = but.DropFrame.Scrolling:FindFirstChild(element)
+				if not el then error("No element found.") return nil end
+				Update(el.Name)
+				el:Destroy()
+			end
+			
+			return Box
+		end
 
 		return Tab
-	end	
+	end
+	
+	
 	return Window
 end
+
 return module
