df7fd8af00749af560f213140799bd0f761ce16b
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-09-24 22:58:22 +0200

    Added Notification

--- UI.lua
+++ UI.lua
@@ -1,13 +1,36 @@
+-- Variables
 local Global = getgenv and getgenv() or _G
+local TweenService = game:GetService("TweenService")
+
+local FSettingTemplate = {
+	["Size"]    = 333,
+	["Visible"] = true,
+	["Color"]   = Color3.fromRGB(255, 0, 127)
+}
+
+local NSettingTemplate = {
+	["Title"]   = "No Title",
+	["Message"] = "No Message",
+	["Time"]    = 5
+}
+
+-- Functions
+
+function TableFind(Table: table, Arg: any)
+	for _,v in pairs(Table) do
+		if v == Arg then return true end
+	end
+	return nil
+end
 
 function TableRemove(tab: table, arg: any)
 	--print("remove", tab)
-    for n,v in pairs(tab) do
-        if v == arg then
+	for n,v in pairs(tab) do
+		if v == arg then
 			--print("removed", v)
 			table.remove(tab, n)
 		end
-    end
+	end
 	return tab
 end
 
@@ -16,14 +39,16 @@ function TableAdd(tab: table, arg: any, pos: number?)
 	if pos then
 		table.insert(tab, arg, pos)
 	else
-	--print("ok for", arg)
-    table.insert(tab, arg)
+		--print("ok for", arg)
+		table.insert(tab, arg)
 	end
 	return tab
 end
 
 module = {}
-function module:CreateGui(name)
+
+
+function module:CreateGui(name: string)
 	local Window = {}
 	local AlreadyLoaded = false
 	for _,v in pairs(game:GetService("CoreGui"):GetChildren()) do
@@ -68,9 +93,19 @@ function module:CreateGui(name)
 	local UICorner_6 = Instance.new("UICorner")
 	local TextLabel = Instance.new("TextLabel")
 	local DropCheck = Instance.new("TextButton")
-    local Check = Instance.new("ImageButton")
-    local CheckCorner = Instance.new("UICorner")
-    local CheckCorner1 = Instance.new("UICorner")
+	local Check = Instance.new("ImageButton")
+	local CheckCorner = Instance.new("UICorner")
+	local CheckCorner1 = Instance.new("UICorner")
+	local NotifScroll = Instance.new("ScrollingFrame")
+	local NotifLayout = Instance.new("UIListLayout")
+	local Notification = Instance.new("Frame")
+	local Text1 = Instance.new("TextLabel")
+	local Cut = Instance.new("Frame")
+	local UIGradient = Instance.new("UIGradient")
+	local UIStroke = Instance.new("UIStroke")
+	local Text2 = Instance.new("TextLabel")
+	local Frame = Instance.new("Frame")
+
 
 	--Properties:
 	local Par = game:GetService("CoreGui")
@@ -80,10 +115,10 @@ function module:CreateGui(name)
 	end
 	if synprotect and not gethui then
 		synprotect(ScreenGui)
-        end
+	end
 	if gethui then
-            Par = gethui()
-        end
+		Par = gethui()
+	end
 
 	ScreenGui.Name = "UH"
 	ScreenGui.Parent = Par
@@ -92,14 +127,85 @@ function module:CreateGui(name)
 	ScreenGui.IgnoreGuiInset = true
 	ScreenGui:SetAttribute("Loaded", true)
 	Global.__UI = ScreenGui
-	
+
 	Hide.Parent = ScreenGui
 	Hide.Size = UDim2.new(1,0,1,0)
 	Hide.ZIndex = 0
 	Hide.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 	Hide.BackgroundTransparency = 0.7
 
+    NotifScroll.Parent = ScreenGui
+	NotifScroll.Name = "NotifScroll"
+	NotifScroll.BackgroundColor3 = Color3.new(0.188235, 0.188235, 0.188235)
+	NotifScroll.BackgroundTransparency = 1
+	NotifScroll.Position = UDim2.new(0.5,0,0,0)
+	NotifScroll.Selectable = false
+	NotifScroll.Size = UDim2.new(0.5,0,1,0)
+	NotifScroll.CanvasSize = UDim2.new(0,0,1,0)
+	NotifScroll.ScrollBarImageColor3 = Color3.new(0, 0, 0)
+	NotifScroll.ScrollBarImageTransparency = 1
+	NotifScroll.ScrollBarThickness = 0
 
+	NotifLayout.Parent = NotifScroll
+	NotifLayout.Name = "NotifLayout"
+	NotifLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
+	NotifLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+	NotifLayout.Padding = UDim.new(0, 10)
+
+	Notification.Name = "Notification"
+	Notification.BackgroundColor3 = Color3.new(0.129412, 0.129412, 0.129412)
+	Notification.BackgroundTransparency = 0.20000000298023224
+	Notification.Position = UDim2.new(0.8340564370155334,0,0.6791545748710632,0)
+	Notification.Size = UDim2.new(0,353,0,127)
+
+	Text1.Parent = Notification
+	Text1.Name = "Text1"
+	Text1.BackgroundColor3 = Color3.new(1, 1, 1)
+	Text1.BackgroundTransparency = 1
+	Text1.BorderColor3 = Color3.new(1, 0, 0.0156863)
+	Text1.Size = UDim2.new(0,353,0,36)
+	Text1.Font = Enum.Font.GothamBold
+	Text1.Text = [[Text1]]
+	Text1.TextColor3 = Color3.new(1, 1, 1)
+	Text1.TextSize = 20
+
+	Cut.Parent = Notification
+	Cut.Name = "Cut"
+	Cut.BackgroundColor3 = Color3.new(1, 0, 0)
+	Cut.BackgroundTransparency = -0.009999999776482582
+	Cut.BorderSizePixel = 0
+	Cut.Position = UDim2.new(0,0,0.28346458077430725,0)
+	Cut.Size = UDim2.new(0,353,0,1)
+
+	UIGradient.Parent = Cut
+	UIGradient.Name = "UIGradient"
+	UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(33, 33, 33)), ColorSequenceKeypoint.new(0.49, Color3.fromRGB(255, 0, 0)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(33, 33, 33))}
+
+	UIStroke.Parent = Notification
+	UIStroke.Name = "UIStroke"
+	UIStroke.Color = Color3.new(0.635294, 0, 0.00784314)
+
+	Text2.Parent = Notification
+	Text2.Name = "Text2"
+	Text2.BackgroundColor3 = Color3.new(1, 1, 1)
+	Text2.BackgroundTransparency = 1
+	Text2.BorderColor3 = Color3.new(1, 0, 0.0156863)
+	Text2.Position = UDim2.new(0,0,0.29133859276771545,0)
+	Text2.Size = UDim2.new(0,353,0,67)
+	Text2.Font = Enum.Font.GothamBold
+	Text2.Text = [[hi guys did you know that]]
+	Text2.TextColor3 = Color3.new(1, 1, 1)
+	Text2.TextScaled = true
+	Text2.TextSize = 20
+	Text2.TextWrapped = true
+	Text2.TextYAlignment = Enum.TextYAlignment.Top
+
+	Frame.Parent = Notification
+	Frame.Name = "Frame"
+	Frame.BackgroundColor3 = Color3.new(0, 0.615686, 0.0509804)
+	Frame.Position = UDim2.new(0.01983002759516239,0,0.8188976645469666,0)
+	Frame.Size = UDim2.new(0,338,0,16)
+	
 	Frames.Name = "Frames"
 	Frames.Parent = ScreenGui
 
@@ -276,7 +382,7 @@ function module:CreateGui(name)
 
 	UICorner_4.CornerRadius = UDim.new(0, 5)
 	UICorner_4.Parent = TextBox_2
-	
+
 	DropBoxTemplate.Name = "DropBoxTemplate"
 	DropBoxTemplate.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	DropBoxTemplate.BackgroundTransparency = 1.000
@@ -305,7 +411,7 @@ function module:CreateGui(name)
 	DropFrame.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
 	DropFrame.Position = UDim2.new(1.11309528, 0, -0.448276401, 0)
 	DropFrame.Size = UDim2.new(0, 177, 0, 100)
-	
+
 	Scrolling.Name = "Scrolling"
 	Scrolling.Parent = DropFrame
 	Scrolling.Active = true
@@ -329,7 +435,7 @@ function module:CreateGui(name)
 	DropButton.Font = Enum.Font.GothamMedium
 	DropButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 	DropButton.TextSize = 14.000
-	
+
 	TextLabel.Parent = Open_3
 	TextBox.Text = "None"
 	TextLabel.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
@@ -352,7 +458,7 @@ function module:CreateGui(name)
 	DropCheck.TextColor3 = Color3.fromRGB(0, 0, 0)
 	DropCheck.TextSize = 14.000
 
-    Check.Name = "check"
+	Check.Name = "check"
 	Check.Parent = DropCheck
 	Check.BackgroundTransparency = 1.000
 	Check.Position = UDim2.new(-0.0315256119, 0, 0, 0)
@@ -363,10 +469,10 @@ function module:CreateGui(name)
 	Check.ImageRectOffset = Vector2.new(312, 4)
 	Check.ImageRectSize = Vector2.new(24, 24)
 
-    CheckCorner.Parent = DropCheck
+	CheckCorner.Parent = DropCheck
 
 	CheckCorner1.Parent = Check
-	
+
 	-- Scripts:
 	local LastPos = 0
 	local First = 1
@@ -384,9 +490,102 @@ function module:CreateGui(name)
 			v.Visible = vis
 		end
 	end)
+	
+	function Window:Notify(Settings: table)
+		task.spawn(function()
+		if type(Settings) ~= "table" then
+			Settings = {
+				["Title"]   = "No Title",
+				["Message"] = "No Message",
+				["Time"]    = 5
+			}
+		end
+		for _,v in pairs(Settings) do
+			if not TableFind(NSettingTemplate, v) then
+				table.insert(Settings, NSettingTemplate[v])
+			end
+		end
+		local no = {}
+		local n = Notification:Clone()
+		n.Parent = NotifScroll
+			n.BackgroundTransparency = 1
+			for _,v in pairs(n:GetChildren()) do
+				if v:IsA("GuiObject") and v.BackgroundTransparency == 0 then
+					v.BackgroundTransparency = 1
+				else
+                    table.insert(no, v)
+				end
+				if v:IsA("TextLabel") then
+					v.TextTransparency = 1
+					continue
+				end
+				v.Transparency = 1
+			end
+					local goal = {}
+			goal.BackgroundTransparency = 0
+			local goal2 = {}
+			goal2.TextTransparency = 0
+			local goal3 = {}
+			goal3.Transparency = 0
+		local tweenInfo = TweenInfo.new(0.5)
+			TweenService:Create(n, tweenInfo, goal):Play()
+			for _,v in pairs(n:GetChildren()) do
+				if v:IsA("GuiObject") and not TableFind(no, v) then
+					TweenService:Create(v, tweenInfo, goal):Play()
+				end
+				if v:IsA("TextLabel") then
+					TweenService:Create(v, tweenInfo, goal2):Play()
+					continue
+				end
+				TweenService:Create(v, tweenInfo, goal3):Play()
+			end
+		n.Frame.Size = UDim2.fromOffset(0, 16)
+		n.Text1.Text = Settings["Title"]
+		n.Text2.Text = Settings["Message"]
+		n.Frame:TweenSize(UDim2.fromOffset(338, 16),
+			Enum.EasingDirection.In,    -- easingDirection (default Out)
+			Enum.EasingStyle.Sine,      -- easingStyle (default Quad)
+			Settings["Time"],                          -- time (default: 1)
+			true                      -- should this tween override ones in-progress? (default: false)
+		)
+		wait(Settings["Time"])
+			local goal = {}
+			goal.BackgroundTransparency = 1
+			local goal2 = {}
+			goal2.TextTransparency = 1
+			local goal3 = {}
+			goal3.Transparency = 1
+		local tweenInfo = TweenInfo.new(0.4)
+			TweenService:Create(n, tweenInfo, goal):Play()
+			for _,v in pairs(n:GetChildren()) do
+				if v:IsA("GuiObject") then
+					TweenService:Create(v, tweenInfo, goal):Play()
+				end
+				if v:IsA("TextLabel") then
+					TweenService:Create(v, tweenInfo, goal2):Play()
+					continue
+				end
+				TweenService:Create(v, tweenInfo, goal3):Play()
+			end
+			wait(tweenInfo.Time)
+			n:Destroy()
+		end)
+	end
 
-	function Window:AddFrame(FName, Size)
+	function Window:AddFrame(FName: string, Settings: table)
 		local Tab = {}
+		if type(Settings) ~= "table" then
+			Settings = {
+				["Size"]    = 333,
+				["Visible"] = true,
+				["Color"]   = Color3.fromRGB(255, 0, 127)
+			}
+		end
+		for _,v in pairs(Settings) do
+			if not TableFind(FSettingTemplate, v) then
+				table.insert(Settings, FSettingTemplate[v])
+			end
+		end
 		local Cop = FrameTemplate:Clone()
 		Cop.Name = FName
 		Cop:FindFirstChild("CategoryText").Text = FName
@@ -398,17 +597,16 @@ function module:CreateGui(name)
 			First = 0
 		end
 		LastPos = Cop.Position
-		if Size ~= nil then
-			Cop:WaitForChild("CategoryFrame").Size = UDim2.fromOffset(Cop:WaitForChild("CategoryFrame").Size.X.Offset, Size)
-		end
+		Cop.BackgroundColor3 = Settings["Color"]
+		Cop:WaitForChild("CategoryFrame").Size = UDim2.fromOffset(Cop:WaitForChild("CategoryFrame").Size.X.Offset, Settings["Size"])
 		Cop:SetAttribute("First", 0)
 		local e = Instance.new("ObjectValue", Cop)
 		e.Name = "LastBut"
 		Cop:WaitForChild("Close").MouseButton1Click:Connect(function()
 			local Rot = Cop:WaitForChild("Close").Rotation + 180
-			
+
 			Cop:WaitForChild("Close").Rotation = Rot
-			
+
 			for _,v in pairs(Cop:WaitForChild("Content"):GetChildren()) do
 				v.Visible = not Cop:WaitForChild("CategoryFrame").Visible
 			end
@@ -484,11 +682,11 @@ function module:CreateGui(name)
 			end
 			Thing:WaitForChild("LastBut").Value = but
 			-- print(Thing:WaitForChild("LastBut").Value)
-			
+
 			but.MouseButton1Click:Connect(function()
 				callback(but.Open.TextBox.Text)
 			end)
-			
+
 			but.Open.MouseButton1Click:Connect(function()
 				but.Open.TextBox.Visible = not but.Open.TextBox.Visible
 			end)
@@ -590,11 +788,11 @@ function module:CreateGui(name)
 			end)
 
 		end
-		
+
 		function Tab:AddDropBox(Name, callback, Check)
 			local Box = {};
 			callback = callback or function() end
-		    if not type(Check) == "boolean" then Check = false end
+			if not type(Check) == "boolean" then Check = false end
 			Thinge = Thing:WaitForChild("Content")
 			local FirstBut = Thing:GetAttribute("First")
 			local LastBut = Thing:WaitForChild("LastBut").Value
@@ -625,7 +823,7 @@ function module:CreateGui(name)
 			end
 			Thing:WaitForChild("LastBut").Value = but
 			-- print(Thing:WaitForChild("LastBut").Value)
-			
+
 			local function Update(New)
 				local TextLab = but.Open.TextLabel
 				local Old     = TextLab.Text
@@ -636,7 +834,7 @@ function module:CreateGui(name)
 				end
 				TextLab.Text = New
 			end
-			
+
 			local function SizeOf(Name, Loc)
 				local siz = 0
 				for _,v in pairs(Loc) do
@@ -644,20 +842,20 @@ function module:CreateGui(name)
 				end
 				return siz
 			end
-			
+
 			but.Open.MouseButton1Click:Connect(function()
 				but.DropFrame.Visible = not but.DropFrame.Visible
 			end)
-			
+
 			function Box:GetSelected()
 				return CurElement
 			end
-			
+
 			but.MouseButton1Click:Connect(function()
 				local selected = Box:GetSelected()
 				callback(selected)
 			end)
-			
+
 			function Box:AddElement(Name, Value)
 				local NewEl = DropButton:Clone()
 				-- Avoid Clones
@@ -672,23 +870,23 @@ function module:CreateGui(name)
 				NewEl.Parent = but.DropFrame.Scrolling
 				NewEl.MouseButton1Click:Connect(function()
 					if Check == false then
-					if but.Open.TextLabel.Text == NewEl.Text then return end
+						if but.Open.TextLabel.Text == NewEl.Text then return end
 					end
 					Update(NewEl.Name)
 					if Check then
 						local fix
 						NewEl.CheckBox.check.Visible = not NewEl.CheckBox.check.Visible
-				        local activ = NewEl.CheckBox.check.Visible
+						local activ = NewEl.CheckBox.check.Visible
 						--print(activ)
 						if activ == true then
 							fix = TableAdd
 						else
 							fix = TableRemove
 						end
-                      if typeof(Value) == "table" then
-						 for _,ok in pairs(Value) do
-							CurElement = fix(CurElement, ok)
-						 end
+						if typeof(Value) == "table" then
+							for _,ok in pairs(Value) do
+								CurElement = fix(CurElement, ok)
+							end
 						else
 							--print(CurElement)
 							CurElement = fix(CurElement, Value)
@@ -696,7 +894,7 @@ function module:CreateGui(name)
 						end
 						--print("new", CurElement)
 					else
-					CurElement = Value
+						CurElement = Value
 					end
 					callback(CurElement)
 				end)
@@ -708,14 +906,14 @@ function module:CreateGui(name)
 				Update(el.Name)
 				el:Destroy()
 			end
-			
+
 			return Box
 		end
 
 		return Tab
 	end
-	
-	
+
+
 	return Window
 end
 
