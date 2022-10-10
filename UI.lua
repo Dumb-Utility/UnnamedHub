cf142bf89632f37d5924277ba7c947d564fec968
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-10-10 21:38:02 +0200

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -894,6 +894,12 @@ function module:CreateGui(name: string)
 			end
 			Thing:WaitForChild("LastBut").Value = but
 			-- print(Thing:WaitForChild("LastBut").Value)
+			local Pages = but.scrolling
+			local layout = Pages:FindFirstAncestorWhichIsA("UIListLayout")
+			Pages.CanvasSize = UDim2.new(0, 0, 0, layout.AbsoluteContentSize.Y)
+                        layout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
+                              Pages.CanvasSize = UDim2.new(0, 0, 0, layout.AbsoluteContentSize.Y)
+                        end)
 
 			local function Update(New)
 				local TextLab = but.Open.TextLabel
