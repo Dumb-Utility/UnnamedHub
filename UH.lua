bb4598a235349418ad242256787c21e74f2edf46
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-11-22 15:11:53 +0100

    Update UH.lua

--- UH.lua
+++ UH.lua
@@ -1,16 +1,2 @@
-local Decryptor = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedStorage/main/Decryptor.lua"))()
-
-
-UH = {}
-
-function UH:Load(T: string)
-    local Source
-    if T:lower() == "beta" then
-        Source = game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedHub/Beta/Loader.lua")
-    else
-        Source = game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedHub/main/Loader.lua")
-    end
-    Decryptor:LoadScript(Source)
-end
-
-return UH
+local Source = game:HttpGet("https://raw.githubusercontent.com/Dumb-Utility/UnnamedHub/main/Loader.lua")
+loadstring(Source)()
